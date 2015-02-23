5aa9d72eda7d935bd6ae6b6ed7829cfc88fb251a
Manuel Spuhler <manuel.spuhler@gmail.com>
2015-02-23 22:10:29 +0100

    Reworked states & results
    Nice states by https://gist.github.com/calebd at
    https://gist.github.com/calebd/93fa347397cec5f88233

--- Queues/ATSerialOperation.swift
+++ Queues/ATSerialOperation.swift
@@ -9,59 +9,91 @@
 import Foundation
 
 typealias Task = (ATSerialOperation -> Void)
+typealias Result = (AnyObject?, NSError?)
 
 class ATSerialOperation: NSOperation {
     
-    override var executing: Bool {
-        get {
-            return _executing
+    enum State {
+        case Ready, Executing, Finished, Cancelled
+        func keyPath() -> String {
+            switch self {
+            case Ready:
+                return "isReady"
+            case Executing:
+                return "isExecuting"
+            case Finished:
+                return "isFinished"
+            case Cancelled:
+                return "isCancelled"
+            }
         }
+    }
+    
+    // MARK: - Public properties
+    
+    var task: Task
+    var _result: Result?
+    var result: Result? {
+        get {return _result}
         set {
-            _executing = newValue
+            _result = newValue
+            state = .Finished
         }
+    }
+    
+    // MARK: - Private properties
+    
+    var state: State {
+        willSet {
+            println("\(state.keyPath()) -> \(newValue.keyPath())")
+            willChangeValueForKey(newValue.keyPath())
+            willChangeValueForKey(state.keyPath())
+        }
+        didSet {
+            didChangeValueForKey(oldValue.keyPath())
+            didChangeValueForKey(state.keyPath())
+        }
+    }
+
+    override var ready: Bool {
+        return state == .Ready
+    }
+    
+    override var executing: Bool {
+        return state == .Executing
     }
     
     override var finished: Bool {
-        get {
-            return _finished
-        }
-        set {
-            _finished = newValue
-        }
+        return state == .Finished
     }
     
-    var task: Task
-    var result: AnyObject?
-    var _executing = false
-    var _finished = false
+    override var cancelled: Bool {
+        return state == .Cancelled
+    }
+    
+    override var asynchronous: Bool {
+        return true
+    }
+    
+    // MARK: - Public methods
     
     init(task: Task) {
         self.task = task
+        self.state = .Ready
+        super.init()
     }
     
     override func start() {
         task(self)
-        
-        self.willChangeValueForKey("isExecuting")
-        executing = true
-        self.didChangeValueForKey("isExecuting")
+        state = .Executing
     }
     
     func finish() {
-        self.willChangeValueForKey("isExecuting")
-        self.willChangeValueForKey("isFinished")
-        executing = false
-        finished = true;
-        self.didChangeValueForKey("isExecuting")
-        self.didChangeValueForKey("isFinished")
+        state = .Finished
     }
     
-    func isFinished() -> Bool {
-        return self.isFinished()
+    override func cancel() {
+        state = .Cancelled
     }
-    
-    func isExecuting() -> Bool {
-        return self.isExecuting()
-    }
-}
 
+}
--- Queues/ViewController.swift
+++ Queues/ViewController.swift
@@ -11,67 +11,76 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var countLabel: UILabel!
+    
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         demo()
     }
     
-    func demo() {
-        println("start")
-        let now = NSDate()
-        
-        let queue = NSOperationQueue()
-        queue.maxConcurrentOperationCount = 1
-        
-        let operation = ATSerialOperation({ task in
-            println("#1 started")
-            let url = NSURL(string: "http://transport.opendata.ch/v1/connections?from=Geneva&to=Zurich")
-            let request = NSURLRequest(URL:url!)
-            let config = NSURLSessionConfiguration.defaultSessionConfiguration()
-            let session = NSURLSession(configuration: config)
-            session.dataTaskWithRequest(request, completionHandler: {(data, response, error) in
-                println("#1 finished")
-                
-                if (error != nil) {
-                    println("#1 Handle error")
-                }
-                
-                var jsonError: NSError?
-                if let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError) as? NSDictionary {
-                    
-                    task.result = json
-                    task.finish()
-                }
-
-            }).resume()
-        })
-        
-        let operation2 = ATSerialOperation({ task in
-            println(operation.result)
-            println("#2 started")
-            NSThread.sleepForTimeInterval(2)
-            task.finish()
-        })
-        
-        operation2.completionBlock = {
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                println("#2 finished")
-                println("isMainThread: \(NSThread.isMainThread())")
-            })
-        }
-
-        queue.addOperations([operation, operation2], waitUntilFinished: false)
-        
-        println("end")
-        let elapsed = NSDate().timeIntervalSinceDate(now)
-        println("isMainThread: \(NSThread.isMainThread()), elapsed: \(elapsed)")
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: Public
+    
+    func demo() {
+        let queue = NSOperationQueue()
+        queue.maxConcurrentOperationCount = 1
+        
+        let operation = ATSerialOperation {task in
+            println("#1 started")
+            self.asyncCall {task.result = $0}
+//          == self.asyncCall {(json, error) in task.result = (json, error)}
+        }
+        
+        let operation2 = ATSerialOperation {task in
+            println("#2 started")
+            
+            if let result = operation.result as? (NSDictionary, NSError) {
+                println("result1: \(result.0.count)")
+            }
+            
+            self.asyncCall {task.result = $0}
+        }
+        
+        operation2.completionBlock = {
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                
+                if let result = operation2.result as? (NSDictionary, NSError) {
+                    println("result2: \(result.0.count)")
+                }
+            })
+        }
+
+        queue.addOperations([operation, operation2], waitUntilFinished: false)
+    }
+    
+    // MARK: async dummy function
+    
+    func asyncCall(block: (json: AnyObject?, error: NSError?) -> Void) {
+        let url = NSURL(string: "http://transport.opendata.ch/v1/connections?from=Geneva&to=Zurich")
+        let request = NSURLRequest(URL:url!)
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        session.dataTaskWithRequest(request, completionHandler: {(data, response, error) in
+            if (error != nil) {
+                block(json: nil, error: error)
+            }
+            
+            var jsonError: NSError?
+            if let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError) as? NSDictionary {
+                
+                if (jsonError != nil) {
+                    block(json: nil, error: jsonError)
+                }
+                
+                block(json: json, error: nil)
+            }
+        }).resume()
+    }
 
 }
-
